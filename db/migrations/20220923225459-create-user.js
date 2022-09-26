6ef1ec4ec3f2954d5d25b39a614c8daeaaa36c16
Alex0711 <alex.senger@hotmail.com>
2022-09-26 16:41:10 -0300

    fix conection and migrations

--- db/migrations/20220923225459-create-user.js
+++ db/migrations/20220923225459-create-user.js
@@ -1,12 +1,18 @@
 'use strict';
-const { UserSchema, USER_TABLE } = require('../models/userModel')
+const { UserSchema, USER_TABLE } = require('../models/userModel');
+const { WalletSchema, WALLET_TABLE } = require('../models/walletModel');
+const { OperationSchema, OPERATION_TABLE } = require('../models/operationModel');
 
 module.exports = {
   async up (queryInterface) {
     await queryInterface.createTable(USER_TABLE, UserSchema);
+    await queryInterface.createTable(WALLET_TABLE, WalletSchema);
+    await queryInterface.createTable(OPERATION_TABLE, OperationSchema);
   },
 
   async down (queryInterface) {
     await queryInterface.dropTable(USER_TABLE);
+    await queryInterface.dropTable(WALLET_TABLE);
+    await queryInterface.dropTable(OPERATION_TABLE);
   }
 };
